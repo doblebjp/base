d30d9b5e70e25c7497a8cfefe8a245591541c187
Benedict <benedict@phpdevel.net>
2014-07-05 15:56:06 +0200

    Point web as document root

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ apt-get update
 # apache
 apt-get install -y apache2
 rm -rf /var/www/html
-ln -fs /vagrant /var/www/html
+ln -fs /vagrant/web /var/www/html
 
 # php
 apt-get install -y php5 php-apc php5-mysql
